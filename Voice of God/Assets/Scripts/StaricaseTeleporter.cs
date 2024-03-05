2a66d64619bc69b2295c23db7271b36d409c920a
BlindSecurity <62887679+Destroyer910@users.noreply.github.com>
2024-03-05 13:24:16 -0500

    Basic implementation

---
+++ Voice of God/Assets/Scripts/StaricaseTeleporter.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaricaseTeleporter : MonoBehaviour
+{
+    [SerializeField] private Vector3 targetPosition;
+}
