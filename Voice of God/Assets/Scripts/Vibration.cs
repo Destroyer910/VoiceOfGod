d93230335ea5bb859c025fb9b47a609d90b21bdc
kizelerr <116563200+kizelerr@users.noreply.github.com>
2024-03-26 13:24:36 -0400

    fdsfdsf

--- Voice of God/Assets/Scripts/Vibration.cs
+++ Voice of God/Assets/Scripts/Vibration.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Vibration : MonoBehaviour
 {
+    XRController xr;
     void Start()
     {
         xr = (XRController) GameObject.FindObjectOfType(typeof(XRController));
     }
-    void Rumble(float amplitude, duration) {
+    void Update() {
+        rumble(1f,0.1f);
+    }
+    void rumble(float amplitude, float duration) {
         xr.SendHapticImpulse(amplitude, duration);
     }
 }
